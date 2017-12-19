461e2a8460b15a12467d0a6e3a1704a71df2cc16
Alexander Mochalin <horroshow@mail.ru>
2017-12-20 02:30:45 +0500

    typed-array-sortedUniq

---
+++ typed-array-sortedUniq/index.js
@@ -0,0 +1,95 @@
+const Benchmark = require('benchmark')
+const sortedUniq = require('lodash/sortedUniq')
+
+function bitCount(n) {
+  n = n - ((n >> 1) & 0x55555555)
+  n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+  return (((n + (n >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24
+}
+
+function sortedUniqUint16Array(array) {
+  var index = -1
+  var length = array.length
+  var seen
+  var pickBits = 0
+
+  while (++index < length) {
+    var value = array[index]
+
+    if (index === 0 || value !== seen) {
+      seen = value
+      pickBits |= 1 << index
+    }
+  }
+  var newSize = bitCount(pickBits)
+  var newArray = new Uint16Array(newSize)
+
+  index = -1
+  var newArrayIndex = -1
+  while (++index < length) {
+    if ((pickBits & (1 << index)) !== 0) {
+      newArray[++newArrayIndex] = array[index]
+    }
+  }
+  return newArray
+}
+
+function sortedUniqUint16ArrayNaive(array) {
+  var index = -1
+  var length = array.length
+  var resIndex = 0
+  var result = []
+  var seen
+
+  while (++index < length) {
+    var value = array[index]
+
+    if (index === 0 || value !== seen) {
+      seen = value
+      result[resIndex++] = value
+    }
+  }
+  return new Uint16Array(result)
+}
+
+function sortedUniqLodash(array) {
+  return new Uint16Array(sortedUniq(array))
+}
+
+function naiveTest(array) {
+  return function() {
+    return sortedUniqUint16ArrayNaive(array)
+  }
+}
+
+function customTest(array) {
+  return function() {
+    return sortedUniqUint16Array(array)
+  }
+}
+
+function lodashTest(array) {
+  return function() {
+    return sortedUniqLodash(array)
+  }
+}
+
+const items = new Uint16Array([10, 20, 30, 30, 40])
+
+const suite = new Benchmark.Suite('sortedUniq')
+
+function onCycle(event) {
+  console.log(String(event.target))
+}
+
+function onComplete() {
+  console.log(this.name, 'Fastest is ' + this.filter('fastest').map('name'))
+}
+
+suite
+  .add('lodash', lodashTest(items))
+  .add('naive', naiveTest(items))
+  .add('custom', customTest(items))
+  .on('cycle', onCycle)
+  .on('complete', onComplete)
+  .run()
